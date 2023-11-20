d7a11bc24c6bba4e8414f0ec36abef7171b8afce
hs <hs@qq.com>
2023-11-20 23:47:11 +0800

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+    // 创建仓库
+    state: {
+        name: '马云'
+    },
+    // 同步调用
+    mutations: {
+
+    },
+    // 异步调用
+    actions: {
+
+    }
+})
---
+++ src/views/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>
+          <common-header></common-header>
+        </el-header>
+        <el-main>
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+import CommonHeader from "../components/CommonHeader.vue";
+
+export default defineComponent({
+  components: {
+    CommonHeader,
+  },
+});
+</script>
